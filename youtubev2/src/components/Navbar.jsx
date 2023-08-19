ea2b32739ad10fd7f64afa6e5729ce26ac73bd4d
developersview <pcslg1998@gmail.com>
2023-08-19 10:35:17 +0530

    creating searchbar

--- youtubev2/src/components/Navbar.jsx
+++ youtubev2/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { logo } from '../utils/Constants'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
     return (
@@ -16,7 +17,10 @@ const Navbar = () => {
                     top: 0,
                     justifyContent: 'space-between'
                 }}>
-
+                <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src={logo} alt="logo" height={45} />
+                </Link>
+                <SearchBar />
             </Stack>
         </>
     )
---
+++ youtubev2/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
+
+
+const SearchBar = () => {
+    return (
+        <>
+            <Paper></Paper>
+        </>
+    )
+}
+
+export default SearchBar
