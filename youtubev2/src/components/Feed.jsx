c281ed9b12ac6be5fcacba9faba87cdd53499587
developersview <pcslg1998@gmail.com>
2023-10-05 01:03:22 +0530

    working on youtube clone

--- youtubev2/src/components/Feed.jsx
+++ youtubev2/src/components/Feed.jsx
@@ -8,14 +8,9 @@ const Feed = () => {
         >
             <Box
                 sx={{
-                    height: {
-                        sx: 'auto', md: '92vh'
-                    },
+                    height: { sx: 'auto', md: '92vh' },
                     borderRight: '1px solid #3d3d3d',
-                    px: {
-                        sx: 0,
-                        md: 2
-                    },
+                    px: { sx: 0, md: 2 }
                 }}
             >
                 <Sidebar />
--- youtubev2/src/components/Sidebar.jsx
+++ youtubev2/src/components/Sidebar.jsx
@@ -4,7 +4,22 @@ import { categories } from '../utils/Constants'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <Stack
+            direction="row"
+            sx={{
+                overflow: 'auto',
+                height: { sx: 'auto', md: '95%' },
+                flexDirection: { md: 'column' }
+            }}
+        >
+            {categories.map((category) => (
+                <button>
+                    <span>{category.icon}</span>
+                    <span>{category.name}</span>
+                </button>
+            ))}
+
+        </Stack>
     )
 }
 
