56be2602c979c14666da0d358b65fc6a820995d1
developersview <pcslg1998@gmail.com>
2023-10-05 00:42:55 +0530

    working on sidebar

---
+++ youtubev2/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { categories } from '../utils/Constants'
+
+const Sidebar = () => {
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+export default Sidebar
