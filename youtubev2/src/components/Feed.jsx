de79bb3e1c596910be4ff5e828fa6e14e960f3d8
developersview <pcslg1998@gmail.com>
2023-10-13 04:24:15 +0530

    working on youtube clone

--- youtubev2/src/components/Feed.jsx
+++ youtubev2/src/components/Feed.jsx
@@ -6,9 +6,14 @@ import { fetchFromAPI } from '../utils/FetchFromAPI';
 
 const Feed = () => {
 
+    const [selectedCategory, setSelectedCategory] = useState('New');
+
+    const [videos, setVideos] = useState([]);
+
     useEffect(() => {
         fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-    }, []);
+            .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
 
     return (
         <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}
@@ -20,7 +25,10 @@ const Feed = () => {
                     px: { sx: 0, md: 2 }
                 }}
             >
-                <Sidebar />
+                <Sidebar
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
                 <Typography
                     className='copyright'
                     variant='body2'
@@ -37,10 +45,10 @@ const Feed = () => {
                     mb={2}
                     sx={{ color: 'white' }}
                 >
-                    New <span style={{ color: '#fc1503' }}>Videos</span>
+                    {selectedCategory} <span style={{ color: '#fc1503' }}>Videos</span>
                 </Typography>
 
-                <Videos videos={[]} />
+                <Videos videos={videos} />
 
             </Box>
 
